42746d930bf8fc2b1150dac96ebf15f7f0ef50a2
Zosia <owsiak@student.agh.edu.pl>
2018-09-20 20:25:32 +0200

    Organised gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,2 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import')
-
-gulp.task('default', function(){
-  console.log("jeje")
-})
-
-
-
-gulp.task('watch', function(){
-
-  watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
-  })
-})
-
-gulp.task('styles', function(){
-  console.log("STYYYYYYYYYYYYLES");
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
-    .pipe(gulp.dest('./app/temp/styles'));
-})
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
